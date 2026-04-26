369630eea3e2b5f8e066756737db8293998ae85a
umohammad <umarmohammad0427@gmail.com>
2026-04-25 17:40:33 -0700

    NAAQS Violations

--- app.py
+++ app.py
@@ -27,19 +27,24 @@ def compute_effects():
 
     base_aq_vectors = compute.vectorize(base_emissions)
     control_aq_vectors = compute.vectorize(modified_emissions)
-    delta_pm = compute.compute_pm(base_aq_vectors) - compute.compute_pm(
-        control_aq_vectors
+    delta_pm = compute.compute_pm(control_aq_vectors) - compute.compute_pm(
+        base_aq_vectors
     )
-    delta_o3 = compute.compute_o3(base_aq_vectors) - compute.compute_o3(
-        control_aq_vectors
+    delta_o3 = compute.compute_o3(control_aq_vectors) - compute.compute_o3(
+        base_aq_vectors
     )
 
     # Compute health impacts using the deltas
     impact = compute.compute_valuation(delta_pm, delta_o3)
 
-    total_costs_by_county = (-jnp.vstack(impact.values()).sum(axis=0)).tolist()
+    total_costs_by_county = (jnp.vstack(impact.values()).sum(axis=0)).tolist()
 
-    return jsonify(total_costs_by_county)
+
+
+    return jsonify({
+        "health_cost_by_county": total_costs_by_county,
+        "naaqs_violations": (jnp.nonzero(compute.compute_pm(control_aq_vectors) > 35)[0] + 1).tolist() #Ambient PM2.5 must not exceed 35μg/m3
+    })
 
 
 if __name__ == '__main__':
